fdf11ad58546391ec96828b4f239da9a2d5fb542
nakanoshachi <kotaro.n2007.6@gmail.com>
2026-07-02 15:03:15 +0900

    add menu

--- Todo.java
+++ Todo.java
@@ -4,24 +4,30 @@ import java.util.Scanner;
 
 public class Todo {
     public static void main(String[] args) {
-        String[] tasks = new String[100]; //タスク　（最大100件）
+        String[] tasks = new String[100]; // タスク （最大100件）
         boolean[] done = new boolean[100]; //
-        int count = 0;                    //件数
+        int count = 0; // 件数
         Scanner sc = new Scanner(System.in);
         while (true) {
             // メニューを表示して番号を読む
             int menu = sc.nextInt();
-            if(menu == 0) {break;}
-            else if (menu == 1) {
-                System.out.print("やること > ");
-                String t = sc.nextLine();        // ヒント：1行の入力を受け取る
-                tasks[count] = t;    // ヒント：入力した t を配列に入れる
-                done[count]  = false;    // ヒント：追加した直後は未完了
-                count++;                   // ヒント：件数を1増やす
-                System.out.println("追加しました");
-            }
-            else if (menu == 2) {/*一覧：第９回 */}
+            if (menu == 0) {
+                System.out.println("タスクはありません");
+            } else if (menu == 1) {
+                add(tasks[count], done[count], count, sc);
+            } else if (menu == 2) {
+                /* 一覧：第９回 */}
             // 3:完了 / 4:削除 は第10回
         }
+    }
+
+    static int add(String[] tasks, boolean[] done, int count, Scanner sc) {
+        System.out.print("やること > ");
+        String t = sc.nextLine();
+        tasks[count] = t; // ヒント：入力した t を配列に入れる
+        done[count] = false; // ヒント：追加した直後は未完了
+        count++; // ヒント：件数を1増やす
+        System.out.println("追加しました");
+        return count + 1;
     }
 }
