c993e4b1980380c3422bdfde1583b563f61d658b
nakanoshachi <kotaro.n2007.6@gmail.com>
2026-07-02 14:53:02 +0900

    2

--- Todo.java
+++ Todo.java
@@ -1,5 +1,7 @@
+
 // Todo.java;
 import java.util.Scanner;
+
 public class Todo {
     public static void main(String[] args) {
         String[] tasks = new String[100]; //タスク　（最大100件）
@@ -8,10 +10,17 @@ public class Todo {
         Scanner sc = new Scanner(System.in);
         while (true) {
             // メニューを表示して番号を読む
-            int choice = sc.nextInt();
-            if(choice == 0) {break;}
-            else if (choice == 1) {/*追加：第９回 */}
-            else if (choice == 2) {/*一覧：第９回 */}
+            int menu = sc.nextInt();
+            if(menu == 0) {break;}
+            else if (menu == 1) {
+                System.out.print("やること > ");
+                String t = sc.nextLine();        // ヒント：1行の入力を受け取る
+                tasks[count] = t;    // ヒント：入力した t を配列に入れる
+                done[count]  = false;    // ヒント：追加した直後は未完了
+                count++;                   // ヒント：件数を1増やす
+                System.out.println("追加しました");
+            }
+            else if (menu == 2) {/*一覧：第９回 */}
             // 3:完了 / 4:削除 は第10回
         }
     }
