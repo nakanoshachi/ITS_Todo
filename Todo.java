9faad5600998fd8eb18a11fb3bd9be3f80e89bf7
nakanoshachi <kotaro.n2007.6@gmail.com>
2026-07-02 15:19:03 +0900

    complete

--- Todo.java
+++ Todo.java
@@ -8,6 +8,7 @@ public class Todo {
         boolean[] done = new boolean[100]; //
         int count = 0; // 件数
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             // メニューを表示して番号を読む
             System.out.println("1:追加  2:一覧  3:完了  4:削除  0:終了");
@@ -19,12 +20,15 @@ public class Todo {
             } else if (menu == 1) {
                 count = add(tasks, done, count, sc);
             } else if (menu == 2) {
-                /* 一覧：第９回 */}
+                list(tasks, done, count);
+            } else if (menu == 3) {
+
+            }
             // 3:完了 / 4:削除 は第10回
         }
     }
 
-    //追加
+    // 追加
     static int add(String[] tasks, boolean[] done, int count, Scanner sc) {
         System.out.print("やること > ");
         String t = sc.nextLine();
@@ -35,19 +39,34 @@ public class Todo {
         return count + 1;
     }
 
-    //一覧
-    static void list(String[] tasks, boolean[] done, int count){
-        if(count == 0){
+    // 一覧
+    static void list(String[] tasks, boolean[] done, int count) {
+        if (count == 0) {
             System.out.println("タスクはありません。");
-        }else{
-            for(int i=0;i<count;i++){
+        } else {
+            for (int i = 0; i < count; i++) {
                 String mark;
-                if(done[i] == true){
+                if (done[i] == true) {
                     mark = "✓";
-                }else{
+                } else {
                     mark = " ";
-                }System.out.println((i+1) + ". [" + mark + "]" + tasks[i]);
+                }
+                System.out.println((i + 1) + ". [" + mark + "]" + tasks[i]);
             }
+        }
+    }
+
+    // 完了
+    static void complete(boolean[] done, int count, Scanner sc) {
+        System.out.print("完了する番号 > ");
+        int n = sc.nextInt();
+        sc.nextInt();
+        if (n >= 1 && n <= count) {
+            done[n-1] = true;
+            // ヒント：done の n-1 番目を完了にする
+            System.out.println("完了にしました");
+        } else {
+            System.out.println("その番号はありません");
         }
     }
 }
