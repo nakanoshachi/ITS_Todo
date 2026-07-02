bdf9fd3a7d5b22f94cda7b13e3a79cbd13c0cb6d
nakanoshachi <kotaro.n2007.6@gmail.com>
2026-07-02 15:03:30 +0900

    add menu

--- Todo.java
+++ Todo.java
@@ -10,17 +10,20 @@ public class Todo {
         Scanner sc = new Scanner(System.in);
         while (true) {
             // メニューを表示して番号を読む
+            System.out.println("1:追加  2:一覧  3:完了  4:削除  0:終了");
+            System.out.print("番号を入力 > ");
             int menu = sc.nextInt();
             if (menu == 0) {
-                System.out.println("タスクはありません");
+                System.out.println("終了します。");
             } else if (menu == 1) {
-                add(tasks[count], done[count], count, sc);
+                count = add(tasks, done, count, sc);
             } else if (menu == 2) {
                 /* 一覧：第９回 */}
             // 3:完了 / 4:削除 は第10回
         }
     }
 
+    //追加
     static int add(String[] tasks, boolean[] done, int count, Scanner sc) {
         System.out.print("やること > ");
         String t = sc.nextLine();
@@ -30,4 +33,6 @@ public class Todo {
         System.out.println("追加しました");
         return count + 1;
     }
+
+    //一覧
 }
