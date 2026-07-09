91a40e1b188514cf82d9f94c6ff4db2cfedd2d6a
nakanoshachi <kotaro.n2007.6@gmail.com>
2026-07-09 15:34:55 +0900

    優先度実装

--- Todo.java
+++ Todo.java
@@ -7,6 +7,7 @@ public class Todo {
         String[] tasks = new String[100]; // タスク （最大100件）
         boolean[] done = new boolean[100]; //
         int count = 0; // 件数
+        int[] priority = new int[100]; // 優先度　1=高 2=中 3=低
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -18,19 +19,19 @@ public class Todo {
                 System.out.println("終了します。");
                 break;
             } else if (menu == 1) {
-                count = add(tasks, done, count, sc);
+                count = add(tasks, done, count, priority, sc);
             } else if (menu == 2) {
-                list(tasks, done, count);
+                list(tasks, done, count, priority);
             } else if (menu == 3) {
                 complete(done, count, sc);
             } else if (menu == 4) {
-                count = delete(tasks, done, count, sc);
+                count = delete(tasks, done, count, priority, sc);
             } else if (menu == 5) {
                 edit(tasks, count, sc);
             } else if (menu == 6) {
-                deleteDone(tasks, done, count);
+                deleteDone(tasks, done, count, priority);
             } else if (menu == 7) {
-                sortUndone(tasks, done, count);
+                sortUndone(tasks, done, count, priority);
             } else if (menu == 8) {
                 search(tasks, count, sc);
             }
@@ -38,25 +39,36 @@ public class Todo {
     }
 
     // 追加
-    static int add(String[] tasks, boolean[] done, int count, Scanner sc) {
+    static int add(String[] tasks, boolean[] done, int count, int[] priority, Scanner sc) {
         if (count == 100) {
             System.out.println("これ以上追加できません。（100件まで）");
             return count;
         }
+
         System.out.print("やること > ");
         String t = sc.next();
         if (t.equals("")) {
             System.out.println("空のタスクは追加できません。");
+            return count;
         }
+
+        System.out.print("優先度 > ");
+        int p = sc.nextInt();
+        if(p!=1&&p!=2&&p!=3){
+            System.out.println("123いずれかの数を入力してください。");
+            return count;
+        }
+
         tasks[count] = t; // ヒント：入力した t を配列に入れる
         done[count] = false; // ヒント：追加した直後は未完了
+        priority[count]=p;
         count++; // ヒント：件数を1増やす
         System.out.println("追加しました");
         return count;
     }
 
     // 一覧
-    static void list(String[] tasks, boolean[] done, int count) {
+    static void list(String[] tasks, boolean[] done, int count, int[] priority) {
         if (count == 0) {
             System.out.println("タスクはありません。");
         } else {
@@ -69,7 +81,11 @@ public class Todo {
                     mark = " ";
                     notDone++;
                 }
-                System.out.println((i + 1) + ". [" + mark + "]" + tasks[i]);
+                String p = "";
+                if(priority[i]==1) p="高";
+                if(priority[i]==2) p="中";
+                if(priority[i]==3) p="低";
+                System.out.println((i + 1) + ". [" + mark + "]" + tasks[i]+",優先度： "+ p);
             }
             System.out.println("未完了" + notDone + " 件 / 全 " + count + "件");
         }
@@ -94,7 +110,7 @@ public class Todo {
     }
 
     // 削除
-    static int delete(String[] tasks, boolean[] done, int count, Scanner sc) {
+    static int delete(String[] tasks, boolean[] done, int count, int[] priority, Scanner sc) {
         System.out.print("削除する番号 > ");
         int n = sc.nextInt();
         sc.nextLine();
@@ -103,6 +119,7 @@ public class Todo {
                 // ヒント：1つ後ろを前へ
                 tasks[i] = tasks[i + 1];
                 done[i] = done[i + 1];
+                priority[i]=priority[i+1];
             }
             count--;
             System.out.println("削除しました");
@@ -125,12 +142,13 @@ public class Todo {
     }
 
     // 完了済み削除
-    static int deleteDone(String[] tasks, boolean[] done, int count) {
+    static int deleteDone(String[] tasks, boolean[] done, int count, int[] priority) {
         for (int i = count - 1; i >= 0; i--) {
             if (done[i] == true) {
                 for (int j = i; j < count - 1; j++) {
                     tasks[j] = tasks[j + 1];
                     done[j] = done[j + 1];
+                    priority[j]=priority[j+1];
                 }
                 count--;
             }
@@ -139,7 +157,7 @@ public class Todo {
     }
 
     // 並び替え未完了上
-    static void sortUndone(String[] tasks, boolean[] done, int count) {
+    static void sortUndone(String[] tasks, boolean[] done, int count, int[] priority) {
         for (int i = 0; i < count; i++) {
             for (int j = 0; j < count - 1; j++) {
                 if (done[j] == true && done[j + 1] == false) {
@@ -150,6 +168,10 @@ public class Todo {
                     boolean dn = done[j];
                     done[j] = done[j + 1];
                     done[j + 1] = dn;
+
+                    int pr = priority[j];
+                    priority[j] = priority[j + 1];
+                    priority[j + 1] = pr;
                 }
             }
         }
